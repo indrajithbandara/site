af3bdd6d4912a4ab66aae5dd387ce5ff262e9542
Héctor Menéndez <etor@gik.mx>
2017-07-09 16:24:58 -0500

    Refactored base footer using flexbox

--- src/template/Footer.jsx
+++ src/template/Footer.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import Style from 'styled-components';
 
 const Component = Style.footer`
-    border-top: 1px solid ${props => props.theme.colorFG};
-    padding-top: 1rem;
-    text-align: center;
+    border-top: 1px solid;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
 
     > h6 {
         margin:0;
         padding:0;
+        opacity: 0.6;
     }
 `;
 
 export default class Footer extends React.Component {
     render = () => <Component>
-        <h6>soy footer</h6>
+        <h6>
+            <strong>️© Héctor Adan Menéndez Rivera,</strong>
+            <span> 1981-{new Date().getFullYear()}.</span>
+            <span> All rights reserved</span>
+        </h6>
     </Component>
 }
