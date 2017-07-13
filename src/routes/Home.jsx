f732efa3ff7c9cb119548e4ac1e4413b22422ac5
Héctor Menéndez <etor@gik.mx>
2017-07-12 19:32:42 -0500

    Base home layout responsive dynamic background image.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,39 +1,71 @@
 import React from 'react';
-import Style from 'styled-components';
+import Style, {keyframes as Keyframes} from 'styled-components';
+import Theme from '../theme';
 
 export default class Home extends React.Component {
     render = () => <Component>
         <figure></figure>
         <aside>
-            Hola Mundo
+            <h2>Hola Mundo</h2>
+            <ul>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+                <li> content </li>
+            </ul>
         </aside>
     </Component>
 }
 
+const colors = Object
+    .keys(Theme)
+    .filter(key => key.indexOf('colorHome') === 0)
+    .map(key => ({ key, val: Theme[key] }))
+    .sort((a, b) => a.key < b.key? -1 : (a.key > b.key? 1 : 0));
+
+const keyframes = Keyframes`${colors
+    // repeat the colors in reverse order so the transition loops infinitely
+    .concat(colors
+        .slice()
+        .reverse()
+        .slice(1) // remove the first element (it would be repeated)
+    )
+    .reduce((res, cur, i, arr) => [
+        res,
+        `${ i * 100 / (arr.length-1) }% {background-color: var(--${cur.key}); }`
+    ].join('\n'), '')}`;
+
 const Component = Style.section`
-    width: 100%;
-    height: 100%;
     display:grid;
-    grid-template-columns: 40% 1fr;
-    grid-template-rows: 1fr;
-    grid-template-areas: "figure" "aside";
-    justify-items: stretch;
-    align-items: stretch;
 
     & > figure {
         display:block;
-        background-color: var(--portrait00);
-        background-image: url('hector-menendez.gif');
+        min-height: calc(100vh - 3rem);
         background-size: 90%;
         background-position-x: 50%;
         background-position-y: 100%;
         background-blend-mode: multiply;
+        background-image: url('hector-menendez.gif');
+        animation: ${keyframes} 30s linear infinite;
         margin:0;
         padding:0;
     }
 
     & > aside {
-        background-color: white;
+        background-color: var(--colorBG-lighter);
         padding: 1em;
     }
+
+    /** Small devices **/
+    @media screen and (max-width: ${prop => prop.theme.sizeScreenSmall}){
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr;
+    }
+
 `;
