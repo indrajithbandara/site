89f3e6a468ce94928f932a7186ed4f5304b04369
Héctor Menéndez <etor@gik.mx>
2017-12-04 05:56:29 -0600

    back: Implemented base hooks for post relations with users.

--- back/src/services/posts/hooks.js
+++ back/src/services/posts/hooks.js
@@ -1,18 +1,45 @@
+import Thrower from '@gik/tools-thrower';
+
+const userVerify = payload => payload.app
+    .service('users')
+    .get(payload.data.user)
+    .catch(() => Thrower('Invalid user', 'PostUserError'))
+    .then(() => payload);
+
+const dateAdd = payload => ({
+    ...payload,
+    data: { ...payload.data, dateAdd: new Date() },
+});
+
+const userGet = payload => new Promise((resolve, reject) => {
+    const users = payload.app.service('users');
+    payload.result.forEach((post, i) => users
+        .get(post.user)
+        .catch(err => reject(
+            Thrower(['Invalid user: %s', err.message], 'PostUserError', false),
+        ))
+        .then((user) => {
+            payload.result[i] = { ...post, user }; // eslint-disable-line no-param-reassign
+            if (i === (payload.result.length - 1)) resolve(payload);
+        }),
+    );
+});
+
 export default {
 
     before: {
         all: [],
         find: [],
         get: [],
-        create: [],
+        create: [userVerify, dateAdd],
         update: [],
-        patch: [],
+        patch: [userVerify],
         remove: [],
     },
 
     after: {
         all: [],
-        find: [],
+        find: [userGet],
         get: [],
         create: [],
         update: [],
